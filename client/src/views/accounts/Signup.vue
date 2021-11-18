383d14dea5e4c32ca3c7444f8fb3730aef0dce3d
고재현 <joali9807@naver.com>
2021-11-18 10:28:24 +0900

    first

---
+++ client/src/views/accounts/Signup.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <h1>Sign up</h1>
+    <div>
+      <label for="username">사용자 이름: </label>
+      <input type="text" id="username" v-model="credentials.username">
+    </div>
+    <div>
+      <label for="password">비밀번호: </label>
+      <input type="password" id="password" v-model="credentials.password">
+    </div>
+    <div>
+      <label for="passwordConfirmation">비밀번호 확인: </label>
+      <input type="password" id="passwordConfirmation" v-model="credentials.passwordConfirmation"
+      @keypress.enter="signup">
+    </div>
+    <button @click="signup">회원가입</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+// const SERVER_URL = process.env.VUE_APP_SERVER_URL
+
+export default {
+  name: 'Singup',
+  data: function () {
+    return {
+      credentials: {
+        username: null,
+        password: null,
+        passwordConfirmation: null,
+      }
+    }
+  },
+  methods: {
+    signup: function () {
+      axios({
+        method: 'post',
+        url: 'http://127.0.0.1:8000/accounts/signup/',
+        data: this.credentials
+      })
+        .then(() => {
+          axios({
+            method: 'post',
+            url: 'http://127.0.0.1:8000/accounts/api-token-auth/',
+            data: this.credentials
+            })
+            .then((res) => {
+              localStorage.setItem('jwt', res.data.token)
+              this.$emit('login')
+              this.$router.push({name: 'TodoList'})
+              })
+        .catch(err => {
+          console.log(err)
+          })
+      })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  },
+}
+</script>
